96479d2a9ca5f45ddf19010a2f80bec0b88a9dd4
Yurii Osiptsov <yosiptsov@Yuriis-MacBook-Pro-2.local>
2025-06-13 20:43:46 +0300

    page object update /4

--- tests/demo.learnwebdriverio.com/hw-locators-from-classes/added-articles-appear-in-feed.spec.ts
+++ tests/demo.learnwebdriverio.com/hw-locators-from-classes/added-articles-appear-in-feed.spec.ts
@@ -50,15 +50,6 @@ test.describe('register a user and add 3 articles', () => {
         addArticlePage.createdArticleLocator,
         'new added articles should be present in the list'
       ).toHaveCount(neededNumberOfArticles);
-
-      // ? how can I refactor this part using imported locators?
-      //expected results - version 2
-      // for (let i = 0; i < neededNumberOfArticles - 1; i++) {
-      //   await expect(
-      //     addArticlePage.createdArticleByNumberLocator(page, timestamp, i),
-      //     `new added article ${i} of ${neededNumberOfArticles - 1} should be visible`
-      //   ).toBeVisible();
-      // }
     }
   );
 });
