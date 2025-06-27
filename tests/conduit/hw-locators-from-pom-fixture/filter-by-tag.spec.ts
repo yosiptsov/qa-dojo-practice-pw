b4c8d2513e78d337e8f4e048f2f5c39097c25227
Yurii Osiptsov <yosiptsov@gmail.com>
2025-06-27 16:41:41 +0300

    complete conduit on using fixtures

--- tests/conduit/hw-locators-from-pom-fixture/filter-by-tag.spec.ts
+++ tests/conduit/hw-locators-from-pom-fixture/filter-by-tag.spec.ts
@@ -9,23 +9,18 @@
 8. Make sure the selected article has needed tag
 */
 
-import { test, expect, Page } from '@playwright/test';
-import { SignUpPage } from '../../../apps/conduitApp/pages/SignUpPage.po';
-import { HomePage } from '../../../apps/conduitApp/pages/HomePage.po';
-import { ArticleDetailsPage } from '../../../apps/conduitApp/pages/ArticleDetailsPage.po'
-
-// baseURL overwrite the baseURL in playwright.config.ts
-const baseURL = 'https://demo.learnwebdriverio.com';
+import { expect, Page } from '@playwright/test';
+import { test } from './fixtures/baseFixture'
+// import { SignUpPage } from '../../../apps/conduitApp/pages/SignUpPage.po';
+// import { HomePage } from '../../../apps/conduitApp/pages/HomePage.po';
+// import { ArticleDetailsPage } from '../../../apps/conduitApp/pages/ArticleDetailsPage.po'
 
 test.describe('filter by tag filters articles in feed properly', () => {
-  test.beforeEach(async ({ page }) => {
-    await page.goto(baseURL);
-  });
 
   test(
     'Demo-1: selecting a tag in section Popular tags should return articles that have this tag',
     { tag: ['@demo', '@HW-classes', '@article'] },
-    async ({ page }) => {
+    async ({ page, signUpPage, homePage, articleDetailsPage}) => {
 
       // preparing test data
       const timestamp = Date.now().toString();
@@ -35,10 +30,6 @@ test.describe('filter by tag filters articles in feed properly', () => {
         password: 'mypassword'
       }
       const tagName = 'demo';
-
-      const signUpPage = new SignUpPage(page);
-      const homePage = new HomePage(page);
-      const articleDetailsPage = new ArticleDetailsPage(page);
   
       // register a new user
       await signUpPage.createUser(user);
@@ -49,6 +40,8 @@ test.describe('filter by tag filters articles in feed properly', () => {
       await homePage.checkIfTagIsPresentInListByTagname(tagName);
 
       await homePage.clickArticleByNumber(0);
+
+      // the next 2 rows are identical. In the first row expect is a part of the class method
       await homePage.checkIfTagIsPresentInListByTagname(tagName);
       //await expect(articleDetailsPage.getTagByName(tagName)).toBeVisible();
     }
