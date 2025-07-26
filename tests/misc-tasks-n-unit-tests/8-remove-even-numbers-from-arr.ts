4700374900d278c666059b6598f78828aafb8957
yosiptsov <yosiptsov@gmail.com>
2025-07-26 21:42:14 +0300

    Create 8-remove-even-numbers-from-arr.ts

---
+++ tests/misc-tasks-n-unit-tests/8-remove-even-numbers-from-arr.ts
@@ -0,0 +1,8 @@
+// TASK: remove all even numbers from an array
+function removeEvenNumbersFromArray(arr: Array<number>) {
+    const result = arr.filter(elem => elem % 2 !== 0);
+    return result;
+}
+
+const array = [3, 5, 4, 2, 1, 6, 8, 7, 9, 5, 6, 9];
+console.log(removeEvenNumbersFromArray(array));
